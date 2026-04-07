65f95f4a3a6caa1c76d9f3533888bd711164f96f
zhiao0731 <48548503+zhiao0731@users.noreply.github.com>
2026-04-08 00:53:40 +0800

    Update __init__.py

--- agentarts/wrapper/tools/code_interpreter/__init__.py
+++ agentarts/wrapper/tools/code_interpreter/__init__.py
@@ -4,6 +4,4 @@ AgentArts Code Interpreter Module
 Provides secure code execution in sandboxed environments.
 """
 
-from .code_interpreter_client import CodeInterpreter, code_session
-
 __all__ = []
